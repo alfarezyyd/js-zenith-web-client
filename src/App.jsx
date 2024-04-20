3f060a53d1c231e7f68958404befed2c73a5a93b
alfarezyyd <adityaalfarezyd@gmail.com>
2024-04-20 17:58:49 +0700

    feat: ProductDetail page

--- src/App.jsx
+++ src/App.jsx
@@ -1,31 +1,13 @@
 import './App.css'
 import MainNavbar from "./components/MainNavbar.jsx";
-import ProductCard from "./components/ProductCard.jsx";
-import ListboxLink from "./components/ListboxLink.jsx";
+import ProductDetail from "./pages/ProductDetail.jsx";
 
 function App() {
 
   return (
     <>
       <MainNavbar/>
-      <div className="flex mt-4">
-        <div className="w-1/3 text-left">
-          <ListboxLink/>
-        </div>
-        <div className="h-56 sm:h-64 xl:h-80 w-2/3 2xl:h-96">
-
-        </div>
-      </div>
-      <div className="xl:mt-14">
-        <h1 className="text-left text-2xl font-semibold subpixel-antialiased">Explore Our Products</h1>
-        <div className="grid xl:grid-cols-4 gap-3.5 mt-5">
-          <ProductCard/>
-          <ProductCard/>
-          <ProductCard/>
-          <ProductCard/>
-          <ProductCard/>
-        </div>
-      </div>
+      <ProductDetail/>
     </>
   )
 }
