5ede0af9b9e79502386dc8c2c3208418731b6e69
alfarezyyd <adityaalfarezyd@gmail.com>
2024-05-05 14:02:10 +0700

    refactor: delete unnecessary attr

--- src/app/layout.js
+++ src/app/layout.js
@@ -16,7 +16,7 @@ export default function RootLayout({children}) {
   return (
     <html lang="en">
     <body
-      className={`${inter.className}, text-white bg-orange-50 min-h-screen`}
+      className={`${inter.className}`}
     >
     <NextUIProvider>
       <main className="dark text-foreground bg-background">
