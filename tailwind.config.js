b660cc8ca904998f639edb04c43d47e30904585d
alfarezyyd <adityaalfarezyd@gmail.com>
2024-05-05 10:00:04 +0700

    feat: add nextUI and flowbite dependency and delete background color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,14 @@
+const {nextui} = require("@nextui-org/react");
+const flowbite = require("flowbite-react/tailwind");
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
     "./src/pages/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+    flowbite.content(),
   ],
   theme: {
     extend: {
@@ -14,5 +19,6 @@ module.exports = {
       },
     },
   },
-  plugins: [],
+  darkMode: "class",
+  plugins: [nextui(), flowbite.plugin()],
 };
