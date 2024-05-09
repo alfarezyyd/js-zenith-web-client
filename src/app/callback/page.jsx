b26dd3375b28702a378cb572d5de882478dd4bd9
alfarezyyd <adityaalfarezyd@gmail.com>
2024-05-09 20:02:36 +0700

    feat: page for handle callback google

---
+++ src/app/callback/page.jsx
@@ -0,0 +1,24 @@
+'use client'
+import {redirect, useSearchParams} from "next/navigation";
+import {useEffect} from "react";
+import useAuthStore from "@/lib/authStore";
+
+export default function Page() {
+
+  const homeParams = useSearchParams();
+  const setAccessToken = useAuthStore((state) => state.setAccessToken);
+
+  useEffect(() => {
+    const token = homeParams.get('token');
+
+    if (token) {
+      setAccessToken(token); // Simpan token ke store menggunakan zustand
+      redirect('http://localhost:3000')
+    }
+
+    // Redirect atau navigasi ke halaman lain
+    // Misalnya, redirect ke halaman utama setelah menyimpan token
+    // Router.push('/'); // Jika menggunakan Next.js Router
+    // history.push('/'); // Jika menggunakan React Router DOM
+  }, [homeParams, setAccessToken]);
+}
