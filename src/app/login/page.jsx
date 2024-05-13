9aef10185c5a663ba476e51c5192fb5598f50cd2
alfarezyyd <adityaalfarezyd@gmail.com>
2024-05-13 23:48:30 +0700

    feat: login page

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -36,46 +36,60 @@ export default function Page() {
     }
   };
   return (
-    <div className="mt-10 h-screen">
-      <div className="flex flex-row">
-        <div className="w-3/6">
-          <Image src="images/side-image.png" alt="logo"/>
-        </div>
-        <div className="w-3/6 p-28 mt-16">
-          <form className="max-w-md mx-auto my-auto" onSubmit={handleSubmit}>
-            <div className="relative z-0 w-full mb-5 group">
-              <input type="email" name="floating_email" id="floating_email"
-                     className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-                     placeholder=" " required
-                     value={email}
-                     onChange={(e) => setEmail(e.target.value)}
-              />
-              <label htmlFor="floating_email"
-                     className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 left-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 rtl:peer-focus:left-auto peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Email
-                address</label>
-            </div>
-            <div className="relative z-0 w-full mb-5 group">
-              <input type="password" name="floating_password" id="floating_password"
-                     value={password}
-                     onChange={(e) => setPassword(e.target.value)}
-                     className="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
-                     placeholder=" " required/>
-              <label htmlFor="floating_password"
-                     className="peer-focus:font-medium absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-6 scale-75 top-3 left-3 -z-10 origin-[0] peer-focus:start-0 rtl:peer-focus:translate-x-1/4 peer-focus:text-blue-600 peer-focus:dark:text-blue-500 peer-placeholder-shown:scale-100 peer-placeholder-shown:translate-y-0 peer-focus:scale-75 peer-focus:-translate-y-6">Password</label>
-            </div>
-            <Button color="primary" type="submit" className="w-full p-5 text-md" radius="sm">
-              Submit
-            </Button>
-            <Button startContent={<FontAwesomeIcon icon={faGoogle}/>} color="" variant="flat" type="submit"
-                    className="w-full mt-5 border-2 p-5 text-md" as={Link}
-                    href={`${process.env.NEXT_PUBLIC_BACKEND_URL}/auth/google`}
-                    radius="sm">
-              Login with Google
-            </Button>
-          </form>
-
+    <section className="bg-gray-50 dark:bg-gray-900">
+      <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
+        <a href="#" className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
+          <img className="w-8 h-8 mr-2" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg" alt="logo"/>
+            Flowbite
+        </a>
+        <div
+          className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
+          <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
+            <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
+              Sign in to your account
+            </h1>
+            <form className="space-y-4 md:space-y-6" action="#">
+              <div>
+                <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your
+                  email</label>
+                <input type="email" name="email" id="email"
+                       className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                       placeholder="name@company.com" required=""/>
+              </div>
+              <div>
+                <label htmlFor="password"
+                       className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Password</label>
+                <input type="password" name="password" id="password" placeholder="••••••••"
+                       className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+                       required=""/>
+              </div>
+              <div className="flex items-center justify-between">
+                <div className="flex items-start">
+                  <div className="flex items-center h-5">
+                    <input id="remember" aria-describedby="remember" type="checkbox"
+                           className="w-4 h-4 border border-gray-300 rounded bg-gray-50 focus:ring-3 focus:ring-primary-300 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-primary-600 dark:ring-offset-gray-800"
+                           required=""/>
+                  </div>
+                  <div className="ml-3 text-sm">
+                    <label htmlFor="remember" className="text-gray-500 dark:text-gray-300">Remember me</label>
+                  </div>
+                </div>
+                <a href="#" className="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">Forgot
+                  password?</a>
+              </div>
+              <button type="submit"
+                      className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign
+                in
+              </button>
+              <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+                Don’t have an account yet? <a href="#"
+                                              className="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign
+                up</a>
+              </p>
+            </form>
+          </div>
         </div>
       </div>
-    </div>
-  )
+        </section>
+)
 }
