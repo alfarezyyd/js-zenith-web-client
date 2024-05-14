5bbb5b2424bc5d8a4a5096c1b3e7589bc23f3cf8
alfarezyyd <adityaalfarezyd@gmail.com>
2024-05-15 06:31:38 +0700

    feat: warning handling

--- src/app/login/page.jsx
+++ src/app/login/page.jsx
@@ -39,8 +39,9 @@ export default function Page() {
     <section className="bg-gray-50 dark:bg-gray-900">
       <div className="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
         <a href="#" className="flex items-center mb-6 text-2xl font-semibold text-gray-900 dark:text-white">
-          <img className="w-8 h-8 mr-2" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg" alt="logo"/>
-            Flowbite
+          <img className="w-8 h-8 mr-2" src="https://flowbite.s3.amazonaws.com/blocks/marketing-ui/logo.svg"
+               alt="logo"/>
+          Flowbite
         </a>
         <div
           className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
@@ -81,6 +82,16 @@ export default function Page() {
                       className="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign
                 in
               </button>
+              <Button type="button" as={Link} href={`${process.env.NEXT_PUBLIC_BACKEND_URL}/auth/google`}
+                      className="w-full text-white bg-[#4285F4] hover:bg-[#4285F4]/90 focus:ring-4 focus:outline-none focus:ring-[#4285F4]/50 font-medium rounded-lg text-sm px-5 py-2.5 mx-auto flex justify-center dark:focus:ring-[#4285F4]/55">
+                <svg className="w-4 h-4 mr-2" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor"
+                     viewBox="0 0 18 19">
+                  <path fillRule="evenodd"
+                        d="M8.842 18.083a8.8 8.8 0 0 1-8.65-8.948 8.841 8.841 0 0 1 8.8-8.652h.153a8.464 8.464 0 0 1 5.7 2.257l-2.193 2.038A5.27 5.27 0 0 0 9.09 3.4a5.882 5.882 0 0 0-.2 11.76h.124a5.091 5.091 0 0 0 5.248-4.057L14.3 11H9V8h8.34c.066.543.095 1.09.088 1.636-.086 5.053-3.463 8.449-8.4 8.449l-.186-.002Z"
+                        clipRule="evenodd"/>
+                </svg>
+                Sign in with Google
+              </Button>
               <p className="text-sm font-light text-gray-500 dark:text-gray-400">
                 Don’t have an account yet? <a href="#"
                                               className="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign
@@ -90,6 +101,6 @@ export default function Page() {
           </div>
         </div>
       </div>
-        </section>
-)
+    </section>
+  )
 }
